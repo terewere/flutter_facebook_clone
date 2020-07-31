44f5dcc7a138ca94b080c74380b60d2fee9d3323
shaibu zachariyya <scodegh@gmail.com>
2020-07-31 12:16:09 +0000

    facebook clone

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_facebook_clone/ui_challenge.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    print(size.width);
+
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.white));
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.white,
+          primaryColorDark: Colors.white,
+          tabBarTheme: TabBarTheme(
+            labelColor: Colors.blue[700],
+            unselectedLabelColor: Colors.black,
+          ),
+          appBarTheme: AppBarTheme(elevation: 0),
+        ),
+        home: DefaultTabController(
+          length: 6,
+          child: Scaffold(
+              appBar: AppBar(
+//                  leading: Text('Facebook'),
+                  primary: true,
+                  title: Text(
+                    'facebook',
+                    style: TextStyle(
+                        color: Colors.blue[700],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 29),
+                  ),
+                  actions: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, right: 10),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, right: 10),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey[200],
+                            child: Icon(
+                              Icons.message,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 10,
+                          top: 10,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            radius: 10,
+                            child: Text(
+                              '2',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                  bottom: TabBar(
+                    tabs: [
+                      Tab(
+                        icon: Icon(
+                          Icons.home,
+                          size: 32,
+                        ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.people,
+                          size: 32,
+                        ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.live_tv,
+                          size: 32,
+                        ),
+                      ),
+                      Tab(
+                        icon: FaIcon(FontAwesomeIcons.flag),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.notifications_none,
+                          size: 32,
+                        ),
+                      ),
+                      Tab(
+                        icon: Icon(
+                          Icons.menu,
+                          size: 32,
+                        ),
+                      ),
+                    ],
+                  )),
+              body: FacbookClone()),
+        ));
+  }
+}
---
+++ lib/ui_challenge.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class FacbookClone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return TabBarView(
+      children: [
+        Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(9),
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage('images/mahmoud.jpg'))),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Text(
+                      "What's on your mind?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    width: size.width / 1.28,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: Colors.grey),
+                        borderRadius: BorderRadius.all(Radius.circular(16))),
+                  )
+                ],
+              ),
+              Divider(
+                thickness: 1,
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(
+                            Icons.videocam,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Text('Live'),
+                      ],
+                    ),
+                    VerticalDivider(
+                      thickness: 1,
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(
+                            Icons.image,
+                            color: Colors.green,
+                          ),
+                        ),
+                        Text('Photo'),
+                      ],
+                    ),
+                    VerticalDivider(
+                      thickness: 1,
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(
+                            Icons.video_call,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        Text('Live'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                thickness: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(9),
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage('images/mahmoud.jpg'))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 9.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Mahmoud Jajah',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Yesterday at 8:01 PM .',
+                                style: TextStyle(color: Colors.black38),
+                              ),
+                              FaIcon(
+                                FontAwesomeIcons.globeAfrica,
+                                size: 17,
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.more_horiz),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Eid Mubarak to all!',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: Image.asset(
+                  'images/eid.jpeg',
+                  width: size.width,
+                  height: 30,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Stack(
+                    overflow: Overflow.visible,
+                    children: [
+                      Positioned(
+                        left: 19,
+                        child: CircleAvatar(
+                          child: FaIcon(
+                            FontAwesomeIcons.solidHeart,
+                            size: 12,
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Colors.red,
+                          radius: 12,
+                        ),
+                      ),
+                      CircleAvatar(
+                        child: Icon(
+                          Icons.thumb_up,
+                          size: 12,
+                          color: Colors.white,
+                        ),
+                        backgroundColor: Colors.blue,
+                        radius: 12,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text('4599k'),
+                  Spacer(),
+                  Text('399k comments . 299k Shares'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 10,
+              ),
+              SizedBox(
+                height: 80,
+              ),
+            ],
+          ),
+        ),
+        Container(),
+        Container(),
+        Container(),
+        Container(),
+        Container(),
+      ],
+    );
+  }
+}
